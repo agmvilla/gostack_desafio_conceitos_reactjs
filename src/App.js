52c4947a9bb760268e850175e7938b2bd286d65f
Andrés Villalobos <agmvilla@gmail.com>
2020-08-27 01:59:40 -0300

    Entrega do desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api"
+import Header from "./components/Header";
 
 import "./styles.css";
 
 function App() {
+  const [repos, setRepos] = useState([])
+
+  useEffect(() =>{
+    api.get('repositories').then( response => {
+      setRepos(response.data)
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: "Andres",
+      url: "https://github.com.br/umarket/react",
+      techs: ['NodeJS', 'reacJS', 'react-native']
+    })
+
+    setRepos([...repos, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepos(repos.filter(
+      repo => repo.id != id
+    ));
   }
 
   return (
     <div>
+      <Header title="Listagem de Repositórios">
+      </Header>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repos.map(repo => 
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
